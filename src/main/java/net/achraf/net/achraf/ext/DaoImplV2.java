3e64746aee50be93889facb61657e7a997283d91
achrafbsibiss <achraf.bsibiss11@gmail.com>
2026-04-19 18:54:30 +0100

    impliment the version to not use the version database add the DAO version 2

---
+++ src/main/java/net/achraf/net/achraf/ext/DaoImplV2.java
@@ -0,0 +1,16 @@
+package net.achraf.net.achraf.ext;
+
+import net.achraf.dao.IDao;
+
+/**
+ * @author macbook
+ **/
+public class DaoImplV2 implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("Version Capture ....");
+        double t = 12;
+        return t;
+    }
+}
