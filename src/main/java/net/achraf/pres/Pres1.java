b84529616910299a3487eedb6f61d2830c6a1697
achrafbsibiss <achraf.bsibiss11@gmail.com>
2026-04-19 18:47:53 +0100

    Add comment for the good practice

--- src/main/java/net/achraf/pres/Pres1.java
+++ src/main/java/net/achraf/pres/Pres1.java
@@ -9,8 +9,8 @@ import net.achraf.metier.MetierImpl;
 public class Pres1 {
     public static void main(String[] args){
         DaoImpl dao = new DaoImpl();
-        MetierImpl  metier = new MetierImpl();
-        metier.setDao(dao);
+        MetierImpl  metier = new MetierImpl(); // good practice make the injection
+//        metier.setDao(dao);  don't use the setter
         System.out.println("Result  = " + metier.getCalcul());
     }
 }
