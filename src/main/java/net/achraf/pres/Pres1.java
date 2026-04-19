a87077a99adfc3101d7fcf0ff5ce47a32c6380a7
achrafbsibiss <achraf.bsibiss11@gmail.com>
2026-04-19 18:56:59 +0100

    impliment the version 2  DAO

--- src/main/java/net/achraf/pres/Pres1.java
+++ src/main/java/net/achraf/pres/Pres1.java
@@ -2,14 +2,15 @@ package net.achraf.pres;
 
 import net.achraf.dao.DaoImpl;
 import net.achraf.metier.MetierImpl;
+import net.achraf.net.achraf.ext.DaoImplV2;
 
 /**
  * @author macbook
  **/
 public class Pres1 {
     public static void main(String[] args){
-        DaoImpl dao = new DaoImpl();
-        MetierImpl  metier = new MetierImpl(); // good practice make the injection
+        DaoImplV2 dao = new DaoImplV2();
+        MetierImpl  metier = new MetierImpl(dao); // good practice make the injection
 //        metier.setDao(dao);  don't use the setter
         System.out.println("Result  = " + metier.getCalcul());
     }
